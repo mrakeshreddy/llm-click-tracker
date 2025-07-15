fc9c5fababaa2e0834a28693a7f84d3a47167f03
mrakeshreddy <61080890+mrakeshreddy@users.noreply.github.com>
2025-07-15 18:25:00 +0530

    Update oauthConfig.js

--- utils/oauthConfig.js
+++ utils/oauthConfig.js
@@ -1,5 +1,5 @@
 export const OAUTH_CONFIG = {
-  client_id: "YOUR_GOOGLE_CLIENT_ID.apps.googleusercontent.com",
-  client_secret: "YOUR_GOOGLE_CLIENT_SECRET",
+  client_id: "764668315789-efimoblb025il5bab5kfud2s8lua9k0d.apps.googleusercontent.com",
+  client_secret: "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",
   redirect_uri: "http://localhost:3000/api/gsc-auth-callback"
 };
