94f13d59d6a6a626be8b2962b9cab66117f4789b
mrakeshreddy <61080890+mrakeshreddy@users.noreply.github.com>
2025-07-15 19:30:35 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -5,7 +5,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyAiMi-gMhXatSUQd-HcD8SO2eG4nsATu4Y",
   authDomain: "llm-click-tracker.firebaseapp.com",
   projectId: "llm-click-tracker",
-  storageBucket: "llm-click-tracker.firebasestorage.app",
+  storageBucket: "llm-click-tracker.appspot.com",
   messagingSenderId: "764668315789",
   appId: "1:764668315789:web:630f387cf74e074a09bac4",
   measurementId: "G-LS1M1KBR8E"
