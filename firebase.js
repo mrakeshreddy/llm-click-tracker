7a32fc2b1a1fc723062f2c4df60cf63b6ddf68ad
mrakeshreddy <61080890+mrakeshreddy@users.noreply.github.com>
2025-07-15 19:28:32 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,15 +1,15 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
-  const firebaseConfig = {
-    apiKey: "AIzaSyAiMi-gMhXatSUQd-HcD8SO2eG4nsATu4Y",
-    authDomain: "llm-click-tracker.firebaseapp.com",
-    projectId: "llm-click-tracker",
-    storageBucket: "llm-click-tracker.firebasestorage.app",
-    messagingSenderId: "764668315789",
-    appId: "1:764668315789:web:716073d8a1cb66aa09bac4",
-    measurementId: "G-MXQEJLYZ6P"
-  };
+const firebaseConfig = {
+  apiKey: "AIzaSyAiMi-gMhXatSUQd-HcD8SO2eG4nsATu4Y",
+  authDomain: "llm-click-tracker.firebaseapp.com",
+  projectId: "llm-click-tracker",
+  storageBucket: "llm-click-tracker.firebasestorage.app",
+  messagingSenderId: "764668315789",
+  appId: "1:764668315789:web:630f387cf74e074a09bac4",
+  measurementId: "G-LS1M1KBR8E"
+};
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
